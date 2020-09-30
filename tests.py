d013d276099ae1fb1e524d9234ebd96c24bf33be
Frederico Costa <>
2020-09-30 16:42:06 +0100

    Added error logging and environment

--- tests.py
+++ tests.py
@@ -1,29 +1,49 @@
+from datetime import datetime
 from db.data_repository import DataRepository
 from api.data_extractor import DataExtractor
+import logging
+from dotenv import load_dotenv
+import os
 
 
+def log_error(type):
+        logging.basicConfig(filename='errors.log',level=logging.DEBUG)
+        logging.info(datetime.now())
+        logging.exception(type)
 
-if __name__ == "__main__":
-    url = 'https://cloud.iexapis.com/stable/stock/FB/batch'
-    params = {'token':'pk_daeacff88d4d4fa58d0a79dbba45d7dd',
-    'types':'chart', 'range': '1m', 'last':10}
-
+def save_stocks(url, params, username,password,address, port,database):
     try:
         dt_ex = DataExtractor(url, params)
         json_response = dt_ex.get_json_response()
         print("Fetching info from API...")
     except Exception as e:
         print("There was an error acessing API:", e)
+        log_error("API")
     else:
         dt_repo = None
         try:
-            dt_repo = DataRepository("root","passroot321","localhost","3306","stock_trade")
+            dt_repo = DataRepository(username,password,address, port,database)
             dt_repo.add_stocks_info(json_response)
         except Exception as e:
             print("There was an error acessing database:", e)
+            log_error("DATABASE")
         finally:
             dt_repo.close_connection()
-        
 
 
-    #dt_repo.get_stocks_info()
+if __name__ == "__main__":
+    load_dotenv()
+    token = os.getenv("TOKEN")
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+    address = os.getenv("ADDRESS")
+    port = os.getenv("PORT")
+    database = os.getenv("DATABASE")
+    url = 'https://cloud.iexapis.com/stable/stock/FB/batch'
+    token = os.getenv("TOKEN")
+    params = {'token':token,
+    'types':'chart', 'range': '1m', 'last':10}
+    
+    save_stocks(url, params, username, password, address, port, database)
+
+
